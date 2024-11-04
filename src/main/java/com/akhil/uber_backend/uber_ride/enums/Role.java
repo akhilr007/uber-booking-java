1729568aa19c16b27f374935f0ab33274bb9ac2f
Akhil Rajan <rajanakhil07@gmail.com>
2024-11-04 22:24:35 +0530

    add User and Role

---
+++ src/main/java/com/akhil/uber_backend/uber_ride/enums/Role.java
@@ -0,0 +1,7 @@
+package com.akhil.uber_backend.uber_ride.enums;
+
+public enum Role {
+    ADMIN,
+    DRIVER,
+    RIDER
+}
---
+++ src/main/java/com/akhil/uber_backend/uber_ride/models/User.java
@@ -0,0 +1,32 @@
+package com.akhil.uber_backend.uber_ride.models;
+
+import com.akhil.uber_backend.uber_ride.enums.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "app_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+}
