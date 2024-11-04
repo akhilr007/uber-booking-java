8fb3a01760bcd7df6751cd9ceaed577bb7f8f222
Akhil Rajan <rajanakhil07@gmail.com>
2024-11-04 20:00:59 +0530

    add docker compose

---
+++ src/main/java/com/akhil/uber_backend/uber_ride/UberRideApplication.java
@@ -0,0 +1,13 @@
+package com.akhil.uber_backend.uber_ride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberRideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberRideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akhil/uber_backend/uber_ride/UberRideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akhil.uber_backend.uber_ride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberRideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
