367697e8bc3cb7a822b6868c46dcdb89f0db5d56
Akhil Rajan <rajanakhil07@gmail.com>
2024-11-04 22:33:35 +0530

    add Rider model

---
+++ src/main/java/com/akhil/uber_backend/uber_ride/models/Rider.java
@@ -0,0 +1,21 @@
+package com.akhil.uber_backend.uber_ride.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Rider {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double rating;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
