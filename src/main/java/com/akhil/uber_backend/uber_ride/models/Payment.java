d522f5dab23ad4ccd34ea0915749585ffffbd08f
Akhil Rajan <rajanakhil07@gmail.com>
2024-11-05 22:07:10 +0530

    add Payment model

---
+++ src/main/java/com/akhil/uber_backend/uber_ride/models/Payment.java
@@ -0,0 +1,34 @@
+package com.akhil.uber_backend.uber_ride.models;
+
+import com.akhil.uber_backend.uber_ride.enums.PaymentMethod;
+import com.akhil.uber_backend.uber_ride.enums.PaymentStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Ride ride;
+
+    private Double amount;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus paymentStatus;
+
+    @CreationTimestamp
+    private LocalDateTime paymentTime;
+}
