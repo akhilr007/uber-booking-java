37b8e08fb1c7ee5d5de944950188b32f46707487
Akhil Rajan <rajanakhil07@gmail.com>
2024-11-05 22:13:26 +0530

    add RideFairCalculationStrategyImpl

---
+++ src/main/java/com/akhil/uber_backend/uber_ride/strategies/impl/RideFairCalculationStrategyImpl.java
@@ -0,0 +1,14 @@
+package com.akhil.uber_backend.uber_ride.strategies.impl;
+
+import com.akhil.uber_backend.uber_ride.dto.RideRequestDTO;
+import com.akhil.uber_backend.uber_ride.strategies.RideFairCalculationStrategy;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RideFairCalculationStrategyImpl implements RideFairCalculationStrategy {
+
+    @Override
+    public double calculateFare(RideRequestDTO rideRequestDTO) {
+        return 0;
+    }
+}
