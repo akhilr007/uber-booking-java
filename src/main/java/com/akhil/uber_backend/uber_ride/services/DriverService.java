2167ca66467aaf56293d9c9e4c87906550925b40
Akhil Rajan <rajanakhil07@gmail.com>
2024-11-05 21:05:32 +0530

    add methods in DriverService interface

--- src/main/java/com/akhil/uber_backend/uber_ride/services/DriverService.java
+++ src/main/java/com/akhil/uber_backend/uber_ride/services/DriverService.java
@@ -1,12 +1,24 @@
 package com.akhil.uber_backend.uber_ride.services;
 
+import com.akhil.uber_backend.uber_ride.dto.DriverDTO;
 import com.akhil.uber_backend.uber_ride.dto.RideDTO;
+import com.akhil.uber_backend.uber_ride.dto.RiderDTO;
+
+import java.util.List;
 
 public interface DriverService {
+
+    RideDTO acceptRide(Long rideId);
 
     RideDTO cancelRide(Long rideId);
 
     RideDTO startRide(Long rideId);
 
     RideDTO endRide(Long rideId);
+
+    RiderDTO rateDriver(Long rideId, Integer rating);
+
+    DriverDTO getMyProfile();
+
+    List<RideDTO> getAllMyRides();
 }
