9af7293f8e5af85d54a738b5b2abd4771083b137
Akhil Rajan <rajanakhil07@gmail.com>
2024-11-05 21:47:31 +0530

    add config for model mapper

---
+++ src/main/java/com/akhil/uber_backend/uber_ride/configs/MapperConfig.java
@@ -0,0 +1,14 @@
+package com.akhil.uber_backend.uber_ride.configs;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
