88413363b475dd912787fcac33e8c8fd50527464
Akhil Rajan <rajanakhil07@gmail.com>
2024-11-05 21:33:19 +0530

    add DistanceServiceOSRMImpl signature

---
+++ src/main/java/com/akhil/uber_backend/uber_ride/services/impl/DistanceServiceOSRMImpl.java
@@ -0,0 +1,12 @@
+package com.akhil.uber_backend.uber_ride.services.impl;
+
+import com.akhil.uber_backend.uber_ride.services.DistanceService;
+import org.locationtech.jts.geom.Point;
+
+public class DistanceServiceOSRMImpl implements DistanceService {
+
+    @Override
+    public double calculateDistance(Point source, Point destination) {
+        return 0;
+    }
+}
