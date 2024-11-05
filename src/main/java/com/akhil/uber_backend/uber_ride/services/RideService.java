e7e2b43910ad7c852453dad56295d6cbcffa3a58
Akhil Rajan <rajanakhil07@gmail.com>
2024-11-05 21:23:15 +0530

    add ride service interface

--- src/main/java/com/akhil/uber_backend/uber_ride/services/RideService.java
+++ src/main/java/com/akhil/uber_backend/uber_ride/services/RideService.java
@@ -1,4 +1,23 @@
 package com.akhil.uber_backend.uber_ride.services;
 
+import com.akhil.uber_backend.uber_ride.dto.RideRequestDTO;
+import com.akhil.uber_backend.uber_ride.enums.RideStatus;
+import com.akhil.uber_backend.uber_ride.models.Driver;
+import com.akhil.uber_backend.uber_ride.models.Ride;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
 public interface RideService {
+
+    Ride getRideById(Long rideId);
+
+    void matchWithDrivers(RideRequestDTO rideRequestDTO);
+
+    Ride createNewRide(RideRequestDTO rideRequestDTO, Driver driver);
+
+    Ride updateRideStatus(Long rideId, RideStatus rideStatus);
+
+    Page<Ride> getAllRidesOfRider(Long riderId, PageRequest pageRequest);
+
+    Page<Ride> getAllRidesOfDriver(Long driverId, PageRequest pageRequest);
 }
