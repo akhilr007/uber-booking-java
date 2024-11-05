ecfdff27b10926150d9bcd97dc88d660b5076f16
Akhil Rajan <rajanakhil07@gmail.com>
2024-11-05 21:35:20 +0530

    add RiderServiceImpl signature

---
+++ src/main/java/com/akhil/uber_backend/uber_ride/services/impl/RiderServiceImpl.java
@@ -0,0 +1,37 @@
+package com.akhil.uber_backend.uber_ride.services.impl;
+
+import com.akhil.uber_backend.uber_ride.dto.DriverDTO;
+import com.akhil.uber_backend.uber_ride.dto.RideDTO;
+import com.akhil.uber_backend.uber_ride.dto.RideRequestDTO;
+import com.akhil.uber_backend.uber_ride.dto.RiderDTO;
+import com.akhil.uber_backend.uber_ride.services.RiderService;
+
+import java.util.List;
+
+public class RiderServiceImpl implements RiderService {
+
+    @Override
+    public RideRequestDTO requestRide(RideRequestDTO rideRequestDTO) {
+        return null;
+    }
+
+    @Override
+    public RideDTO cancelRide(Long rideId) {
+        return null;
+    }
+
+    @Override
+    public DriverDTO rateDriver(Long rideId, Integer rating) {
+        return null;
+    }
+
+    @Override
+    public RiderDTO getMyProfile() {
+        return null;
+    }
+
+    @Override
+    public List<RideDTO> getAllMyRides() {
+        return List.of();
+    }
+}
