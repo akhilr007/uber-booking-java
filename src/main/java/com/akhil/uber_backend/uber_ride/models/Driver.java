3a39eb09f213ae1516bf8121ca1cda066133c482
Akhil Rajan <rajanakhil07@gmail.com>
2024-11-05 14:14:21 +0530

    add Driver model

---
+++ src/main/java/com/akhil/uber_backend/uber_ride/models/Driver.java
@@ -0,0 +1,27 @@
+package com.akhil.uber_backend.uber_ride.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.locationtech.jts.geom.Point;
+
+@Entity
+@Getter
+@Setter
+public class Driver {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double rating;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private Boolean available;
+
+    @Column(columnDefinition = "Geometry(Point, 4326)")
+    private Point currentLocation;
+}
