e363a21677bc21a0981c65d8752f926292d3180b
Akhil Rajan <rajanakhil07@gmail.com>
2024-11-11 21:01:31 +0530

    modify ride repository

--- src/main/java/com/akhil/uber_backend/uber_ride/repositories/RideRepository.java
+++ src/main/java/com/akhil/uber_backend/uber_ride/repositories/RideRepository.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RideRepository extends JpaRepository<Ride, Long> {
-    
+
     Page<Ride> findByRider(Rider rider, PageRequest pageRequest);
 
     Page<Ride> findByDriver(Driver driver, PageRequest pageRequest);
